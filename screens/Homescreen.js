5bacddc2ffaa9334b9e35cb4b6fb9688490e76db
prince-konwea <kokodecontee123@gmail.com>
2023-05-24 02:21:40 -0700

    adding search, user and adjustment icons to header

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,4 +1,4 @@
-import {View, Text, Image} from "react-native";
+import {View, Text, Image, TextInput} from "react-native";
 import React, {useLayoutEffect} from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -22,7 +22,7 @@ const HomeScreen = () => {
    }, [])
 
     return (
-        <SafeAreaView>
+        <SafeAreaView className="bg-white pt-5">
              <Text className="text-red-500">
             <View className="flex-row pb-3 items-center mx-4 space-x-2">
                 {/* Header */}
@@ -33,11 +33,29 @@ const HomeScreen = () => {
                  }}
                  className = "h-7 w-7 bg-gray-300 p-4 rounded-full"
                 />
-                <View>
+                <View className="flex-1">
                     <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
                     <Text className="font-bold text-nxl">Current Location</Text>
                     <ChevronDownIcon size={20} color="#00CCBB" />
+                    
                   </View>
+
+                  <UserIcon size={35} color="#00CCBB" />
+            </View>
+
+            {/* search */}
+
+            <View>
+                <View className="flex-row space-x-2 flex-1">
+                <SearchIcon />
+                <TextInput 
+                placeholder="Restaurants and Cuisines"
+                keyboardType="Default"
+                />
+                </View>
+
+
+                <AdjustmentsIcon color="#00CCBB" />
             </View>
             </Text>
         </SafeAreaView>
